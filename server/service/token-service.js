1a41df1c775d5ccbf457af0c2fb14896e1d67cc8
viktor-rumiievskyi <vityok05042002@gmail.com>
2022-11-01 16:06:54 +0200

    User registration

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,19 @@
-class TokenService {
+const UserModel = require('../models/user-models');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
 
+class TokenService {
+	async registration(email, password) {
+		const candidate = await UserModel.findOne({email})
+		if (candidate) {
+			throw new Error (`Użytkownik z adresem pocztowym ${email} już istnieje`)
+		}
+		const hashPassword = await bcrypt.hash(password, 3);
+		const activationLink =  uuid.v4();
+		const user = await UserModel.create({email, password: hashPassword, activationLink});
+		await mailService.sendActivationMail(mail, activationLink);
+	}
 }
 
 module.exports =  new TokenService();
