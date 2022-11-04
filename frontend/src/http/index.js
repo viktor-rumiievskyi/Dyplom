bbd5a31db897d11efb2a4761e28d4d6f7316a39b
viktor-rumiievskyi <vityok05042002@gmail.com>
2022-11-04 14:39:15 +0200

    Interceptors and adding a token to the request

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -5,3 +5,9 @@ const  $api = axios.create({
 	withCredentials: true,
 	baseURL: API_URL,
 })
+
+$api.interceptors.request.use((config) => {
+	config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+	return config;
+})
+
